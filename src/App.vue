298abcd96dc8a709e08253b7c28836c7a4e8d894
qwfy5287 <qwfy5287@qq.com>
2021-05-06 15:25:19 +0800

    vue2 enter char example

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+    <h2>example 1</h2>
+    <div>
+      <span>aa</span>
+      <span>bb</span>
+    </div>
+    <h2>example 2</h2>
+    <div><span>aa</span><span>bb</span></div>
+  </div>
+</template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
